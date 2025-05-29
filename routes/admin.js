1d67e992add18c002521b23a7f9d16beeb7c744c
조우진 <steamedbunwith@gmail.com>
2025-05-29 16:57:54 +0900

    feat: revoke user access

--- routes/admin.js
+++ routes/admin.js
@@ -12,6 +12,14 @@ router.post(
   adminController.grantAccess
 );
 
+// 특정 유저에게 접근 권한 취소
+router.delete(
+  "/revoke-access",
+  authenticateToken,
+  isAdmin,
+  adminController.revokeAccess
+);
+
 // 관리자 주문 취소 요청 승인
 router.delete(
   "/orders/:orderId/cancel",
